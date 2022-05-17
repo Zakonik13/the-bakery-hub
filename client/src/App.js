5aca2d86083ea90c948229b5d6ac5a35c07ed500
zakonik <zakonik2417@gmail.com>
2022-05-17 12:22:34 -0400

    removed localhost

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ import Admin from "./components/Admin";
 import Signup from "./pages/Signup";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
